bd4436739b6df27c2a9cb4fc543b66d2c00b75fc
varsha-santhanam-2 <136883597+varsha-santhanam-2@users.noreply.github.com>
2025-01-28 22:47:59 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+// Smooth Scrolling
+document.querySelectorAll('.nav ul li a').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        const target = document.querySelector(this.getAttribute('href'));
+        if (target) {
+            target.scrollIntoView({
+                behavior: 'smooth',
+                block: 'start'
+            });
+        }
+    });
+});
